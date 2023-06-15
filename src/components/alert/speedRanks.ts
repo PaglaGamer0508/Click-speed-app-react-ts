a501f958c441cc7ff06068f093d3c023f1104b46
PaglaGamer <PaglaGamer@fahadmahmud.com>
2023-06-15 13:35:09 +0600

    unneccessary image removed

--- src/components/alert/speedRanks.ts
+++ src/components/alert/speedRanks.ts
@@ -4,44 +4,31 @@ import Luffy from "../../images/luffy.jpg";
 import Naruto from "../../images/naruto.jpg";
 import Goku from "../../images/goku.jpg";
 import DragonBalls from "../../images/DragonBalls.jpg";
-// Small images
-import Hayate_small from "../../images/small-image/hayate.jpg";
-import Deku_small from "../../images/small-image/deku.jpg";
-import Luffy_small from "../../images/small-image/luffy.jpg";
-import Naruto_small from "../../images/small-image/naruto.jpg";
-import Goku_small from "../../images/small-image/goku.jpg"
-import DragonBalls_small from "../../images/small-image/DragonBalls.jpg";
 
 const speedRanks = [
   {
     name: "That guy from Naruto, but not one of the other 2 guys",
-    image: Hayate,
-    small_image: Hayate_small,
+    image: Hayate
   },
   {
     name: "Izuku Midoriya (Deku)",
-    image: Deku,
-    small_image: Deku_small,
+    image: Deku
   },
   {
     name: "Monkey D. Luffy",
-    image: Luffy,
-    small_image: Luffy_small,
+    image: Luffy
   },
   {
     name: "Naruto Uzumaki",
-    image: Naruto,
-    small_image: Naruto_small,
+    image: Naruto
   },
   {
     name: "Goku (Kakarot)",
-    image: Goku,
-    small_image: Goku_small,
+    image: Goku
   },
   {
     name: "You have reached the highest level you can have the Dragon Balls as a reward",
-    image: DragonBalls,
-    small_image: DragonBalls_small,
+    image: DragonBalls
   },
 ];
 
